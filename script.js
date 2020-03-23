ff26e7bb1792da152efcc56eb1d6696b13f233fd
Alexandra <sgrjaznova@gmail.com>
2020-03-23 18:36:07 +0200

    Script, css and html files

---
+++ script.js
@@ -0,0 +1,38 @@
+const formSearch = document.querySelector('.form-search'),
+      inputCitiesFrom = formSearch.querySelector('.input__cities-from'),
+      dropdownCitiesFrom = formSearch.querySelector('.dropdown__cities-from'),
+      inputCitiesTo = formSearch.querySelector('.input__cities-to'),
+      dropdownCitiesTo = formSearch.querySelector('.dropdown__cities-to'),
+      inputDateDepart = formSearch.querySelector('.input__date-depart')
+      ;
+
+const city = [
+  'New-York', 'Minsk', 'Kyiv',
+  'Warsaw', 'Prague', 'Paris', 'Munich',
+  'Berlin', 'Strasbourg', 'London', 'Odessa',
+  'Wien', 'Ankara', 'Sofia'
+];
+
+const showCity = (input, list) => {
+  list.textContent = '';
+
+  if (input.value == '') return;
+
+  const filteredCity = city.filter((item) => {
+    const fixedItem = item.toLowerCase();
+
+    return fixedItem.includes(input.value.toLowerCase());
+  });
+
+  filteredCity.forEach((item) => {
+    const li = document.createElement('li');
+    li.classList.add('dropdown__city');
+    li.textContent = item;
+    list.append(li)
+  });
+};
+
+inputCitiesFrom.addEventListener('input', () => {
+  showCity(inputCitiesFrom, dropdownCitiesFrom)
+});
+
